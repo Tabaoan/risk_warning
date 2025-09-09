9ecf28457780d474be8ccf6e2db1d73f66bee163
Ta An <tabaoan11@gmail.com>
2025-09-09 15:03:18 +0700

    baoan

--- app.py
+++ app.py
@@ -344,7 +344,7 @@ with gr.Blocks(title="Credit Risk & Score Assessment", theme=gr.themes.Soft()) a
 if __name__=="__main__":
     port = int(os.environ.get('PORT', 7860))
     demo.launch(
-        server_name='127.0.0.1',
+        server_name='0.0.0.0',
         server_port=port,
         share=False
     )
